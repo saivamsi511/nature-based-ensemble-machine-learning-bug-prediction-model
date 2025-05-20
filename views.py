712642a97f1537f91ed04297ccce1562aab30883
saivamsi511 <131231330+saivamsi511@users.noreply.github.com>
2025-05-20 11:50:56 +0530

    Add files via upload

--- views.py
+++ views.py
@@ -1,224 +1,259 @@
+
+from django.db.models import  Count, Avg
+from django.shortcuts import render, redirect
 from django.db.models import Count
 from django.db.models import Q
-from django.shortcuts import render, redirect, get_object_or_404
+import datetime
+import xlwt
+from django.http import HttpResponse
+
+
+import string
+
 
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
 from sklearn.feature_extraction.text import CountVectorizer
 
+
 from sklearn.tree import DecisionTreeClassifier
-
-from sklearn.ensemble import VotingClassifier
 #model selection
-from sklearn.metrics import confusion_matrix, accuracy_score, plot_confusion_matrix, classification_report
+from sklearn.metrics import confusion_matrix, accuracy_score,classification_report
+
 # Create your views here.
-
-
-
-
 from Remote_User.models import ClientRegister_Model,bug_report_prediction,detection_ratio,detection_accuracy
 
-def login(request):
 
-
-    if request.method == "POST" and 'submit1' in request.POST:
-
-        username = request.POST.get('username')
+def serviceproviderlogin(request):
+    if request.method  == "POST":
+        admin = request.POST.get('username')
         password = request.POST.get('password')
-        try:
-            enter = ClientRegister_Model.objects.get(username=username,password=password)
-            request.session["userid"] = enter.id
+        if admin == "Admin" and password =="Admin":
+            detection_accuracy.objects.all().delete()
+            return redirect('View_Remote_Users')
 
-            return redirect('ViewYourProfile')
-        except:
-            pass
+    return render(request,'SProvider/serviceproviderlogin.html')
 
-    return render(request,'RUser/login.html')
+def View_Prediction_Of_Bug_Report_Type_Ratio(request):
+    detection_ratio.objects.all().delete()
+    rratio = ""
+    kword = 'normal'
+    print(kword)
+    obj = bug_report_prediction.objects.all().filter(Q(Prediction=kword))
+    obj1 =bug_report_prediction.objects.all()
+    count = obj.count();
+    count1 = obj1.count();
+    ratio = (count / count1) * 100
+    if ratio != 0:
+        detection_ratio.objects.create(names=kword, ratio=ratio)
 
-def Register1(request):
-    if request.method == "POST":
-        username = request.POST.get('username')
-        email = request.POST.get('email')
-        password = request.POST.get('password')
-        phoneno = request.POST.get('phoneno')
-        country = request.POST.get('country')
-        state = request.POST.get('state')
-        city = request.POST.get('city')
-        address = request.POST.get('address')
-        gender = request.POST.get('gender')
-        ClientRegister_Model.objects.create(username=username, email=email, password=password, phoneno=phoneno,
-                                            country=country, state=state, city=city, address=address, gender=gender)
-        obj = "Registered Successfully"
-        return render(request, 'RUser/Register1.html', {'object': obj})
-    else:
-        return render(request,'RUser/Register1.html')
+    ratio1 = ""
+    kword1 = 'blocker'
+    print(kword1)
+    obj1 = bug_report_prediction.objects.all().filter(Q(Prediction=kword1))
+    obj11 = bug_report_prediction.objects.all()
+    count1 = obj1.count();
+    count11 = obj11.count();
+    ratio1 = (count1 / count11) * 100
+    if ratio1 != 0:
+        detection_ratio.objects.create(names=kword1, ratio=ratio1)
 
-def ViewYourProfile(request):
-    userid = request.session['userid']
-    obj = ClientRegister_Model.objects.get(id= userid)
-    return render(request,'RUser/ViewYourProfile.html',{'object':obj})
+    ratio12 = ""
+    kword12 = 'major'
+    print(kword12)
+    obj12 = bug_report_prediction.objects.all().filter(Q(Prediction=kword12))
+    obj112 = bug_report_prediction.objects.all()
+    count12 = obj12.count();
+    count112 = obj112.count();
+    ratio12 = (count12 / count112) * 100
+    if ratio12 != 0:
+        detection_ratio.objects.create(names=kword12, ratio=ratio12)
 
-
-def Predict_Bug_Report_Type(request):
-    if request.method == "POST":
-
-        if request.method == "POST":
-
-            bug_id= request.POST.get('bug_id')
-            creation_date= request.POST.get('creation_date')
-            component_name= request.POST.get('component_name')
-            product_name= request.POST.get('product_name')
-            short_description= request.POST.get('short_description')
-            long_description= request.POST.get('long_description')
-            reporter_name= request.POST.get('reporter_name')
-            resolution_category= request.POST.get('resolution_category')
-            status_category= request.POST.get('status_category')
-            bug_fix_time= request.POST.get('bug_fix_time')
-
-        df = pd.read_csv('Datasets.csv')
-
-        # data under nlp
-        print("Data Processing Under Natural Language Processing (NLP)")
-        data = []
-        Labels = []
-        
-
-        def apply_results(results):
-            if (results == 'normal'):
-                return 0
-            elif (results == 'blocker'):
-                return 1
-            elif (results == 'major'):
-                return 2
-            elif (results == 'minor'):
-                return 3
+    ratio123 = ""
+    kword123 = 'minor'
+    print(kword123)
+    obj123 = bug_report_prediction.objects.all().filter(Q(Prediction=kword123))
+    obj1123 = bug_report_prediction.objects.all()
+    count123 = obj123.count();
+    count1123 = obj1123.count();
+    ratio123 = (count123 / count1123) * 100
+    if ratio123 != 0:
+        detection_ratio.objects.create(names=kword123, ratio=ratio123)
 
 
-        df['results'] = df['severity_category'].apply(apply_results)
+    obj = detection_ratio.objects.all()
+    return render(request, 'SProvider/View_Prediction_Of_Bug_Report_Type_Ratio.html', {'objs': obj})
 
-        X = df['short_description']
-        y = df['results']
+def View_Remote_Users(request):
+    obj=ClientRegister_Model.objects.all()
+    return render(request,'SProvider/View_Remote_Users.html',{'objects':obj})
 
-        cv = CountVectorizer()
-        x = cv.fit_transform(X)
+def ViewTrendings(request):
+    topic = bug_report_prediction.objects.values('topics').annotate(dcount=Count('topics')).order_by('-dcount')
+    return  render(request,'SProvider/ViewTrendings.html',{'objects':topic})
 
-        models = []
-        from sklearn.model_selection import train_test_split
-        X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.20)
-        X_train.shape, X_test.shape, y_train.shape
+def charts(request,chart_type):
+    chart1 = detection_ratio.objects.values('names').annotate(dcount=Avg('ratio'))
+    return render(request,"SProvider/charts.html", {'form':chart1, 'chart_type':chart_type})
 
-        print("Naive Bayes")
+def charts1(request,chart_type):
+    chart1 = detection_accuracy.objects.values('names').annotate(dcount=Avg('ratio'))
+    return render(request,"SProvider/charts1.html", {'form':chart1, 'chart_type':chart_type})
 
-        from sklearn.naive_bayes import MultinomialNB
+def View_Prediction_Of_Bug_Report_Type(request):
+    obj =bug_report_prediction.objects.all()
+    return render(request, 'SProvider/View_Prediction_Of_Bug_Report_Type.html', {'list_objects': obj})
 
-        NB = MultinomialNB()
-        NB.fit(X_train, y_train)
-        predict_nb = NB.predict(X_test)
-        naivebayes = accuracy_score(y_test, predict_nb) * 100
-        print("ACCURACY")
-        print(naivebayes)
-        print("CLASSIFICATION REPORT")
-        print(classification_report(y_test, predict_nb))
-        print("CONFUSION MATRIX")
-        print(confusion_matrix(y_test, predict_nb))
-        models.append(('naive_bayes', NB))
-
-        # SVM Model
-        print("SVM")
-        from sklearn import svm
-
-        lin_clf = svm.LinearSVC()
-        lin_clf.fit(X_train, y_train)
-        predict_svm = lin_clf.predict(X_test)
-        svm_acc = accuracy_score(y_test, predict_svm) * 100
-        print("ACCURACY")
-        print(svm_acc)
-        print("CLASSIFICATION REPORT")
-        print(classification_report(y_test, predict_svm))
-        print("CONFUSION MATRIX")
-        print(confusion_matrix(y_test, predict_svm))
-        models.append(('SVM', lin_clf))
-
-        print("Logistic Regression")
-
-        from sklearn.linear_model import LogisticRegression
-
-        reg = LogisticRegression(random_state=0, solver='lbfgs').fit(X_train, y_train)
-        y_pred = reg.predict(X_test)
-        print("ACCURACY")
-        print(accuracy_score(y_test, y_pred) * 100)
-        print("CLASSIFICATION REPORT")
-        print(classification_report(y_test, y_pred))
-        print("CONFUSION MATRIX")
-        print(confusion_matrix(y_test, y_pred))
-        models.append(('LogisticRegression', reg))
-
-        print("Decision Tree Classifier")
-        dtc = DecisionTreeClassifier()
-        dtc.fit(X_train, y_train)
-        dtcpredict = dtc.predict(X_test)
-        print("ACCURACY")
-        print(accuracy_score(y_test, dtcpredict) * 100)
-        print("CLASSIFICATION REPORT")
-        print(classification_report(y_test, dtcpredict))
-        print("CONFUSION MATRIX")
-        print(confusion_matrix(y_test, dtcpredict))
-        models.append(('DecisionTreeClassifier', dtc))
-
-        print("KNeighborsClassifier")
-        from sklearn.neighbors import KNeighborsClassifier
-        kn = KNeighborsClassifier()
-        kn.fit(X_train, y_train)
-        knpredict = kn.predict(X_test)
-        print("ACCURACY")
-        print(accuracy_score(y_test, knpredict) * 100)
-        print("CLASSIFICATION REPORT")
-        print(classification_report(y_test, knpredict))
-        print("CONFUSION MATRIX")
-        print(confusion_matrix(y_test, knpredict))
-        models.append(('KNeighborsClassifier', kn))
-
-        classifier = VotingClassifier(models)
-        classifier.fit(X_train, y_train)
-        y_pred = classifier.predict(X_test)
-
-        short_description1 = [short_description]
-        vector1 = cv.transform(short_description1).toarray()
-        predict_text = classifier.predict(vector1)
-
-        pred = str(predict_text).replace("[", "")
-        pred1 = str(pred.replace("]", ""))
-
-        prediction = int(pred1)
-
-        if (prediction == 0):
-            val = 'normal'
-        elif (prediction == 1):
-            val = 'blocker'
-        elif (prediction == 2):
-            val = 'major'
-        elif (prediction == 3):
-            val = 'minor'
+def likeschart(request,like_chart):
+    charts =detection_accuracy.objects.values('names').annotate(dcount=Avg('ratio'))
+    return render(request,"SProvider/likeschart.html", {'form':charts, 'like_chart':like_chart})
 
 
-        print(prediction)
-        print(val)
+def Download_Predicted_DataSets(request):
 
-        bug_report_prediction.objects.create(
-        bug_id=bug_id,
-        creation_date=creation_date,
-        component_name=component_name,
-        product_name=product_name,
-        short_description=short_description,
-        long_description=long_description,
-        reporter_name=reporter_name,
-        resolution_category=resolution_category,
-        status_category=status_category,
-        bug_fix_time=bug_fix_time,
-        Prediction=val)
+    response = HttpResponse(content_type='application/ms-excel')
+    # decide file name
+    response['Content-Disposition'] = 'attachment; filename="Predicted_Data.xls"'
+    # creating workbook
+    wb = xlwt.Workbook(encoding='utf-8')
+    # adding sheet
+    ws = wb.add_sheet("sheet1")
+    # Sheet header, first row
+    row_num = 0
+    font_style = xlwt.XFStyle()
+    # headers are bold
+    font_style.font.bold = True
+    # writer = csv.writer(response)
+    obj = bug_report_prediction.objects.all()
+    data = obj  # dummy method to fetch data.
+    for my_row in data:
+        row_num = row_num + 1
 
-        return render(request, 'RUser/Predict_Bug_Report_Type.html',{'objs': val})
-    return render(request, 'RUser/Predict_Bug_Report_Type.html')
+        ws.write(row_num, 0, my_row.bug_id, font_style)
+        ws.write(row_num, 1, my_row.creation_date, font_style)
+        ws.write(row_num, 2, my_row.component_name, font_style)
+        ws.write(row_num, 3, my_row.product_name, font_style)
+        ws.write(row_num, 4, my_row.short_description, font_style)
+        ws.write(row_num, 5, my_row.long_description, font_style)
+        ws.write(row_num, 6, my_row.reporter_name, font_style)
+        ws.write(row_num, 7, my_row.resolution_category, font_style)
+        ws.write(row_num, 8, my_row.status_category, font_style)
+        ws.write(row_num, 9, my_row.bug_fix_time, font_style)
+        ws.write(row_num, 10, my_row.Prediction, font_style)
+
+    wb.save(response)
+    return response
+
+def train_model(request):
+    detection_accuracy.objects.all().delete()
+    df = pd.read_csv('Datasets.csv')
+
+    # data under nlp
+    print("Data Processing Under Natural Language Processing (NLP)")
+  
+    def apply_results(results):
+        if (results == 'normal'):
+            return 0
+        elif (results == 'blocker'):
+            return 1
+        elif (results == 'major'):
+            return 2
+        elif (results == 'minor'):
+            return 3
 
 
 
+    df['results'] = df['severity_category'].apply(apply_results)
+
+    X = df['short_description']
+    y = df['results']
+
+
+    cv = CountVectorizer()
+    x = cv.fit_transform(X)
+
+
+    models = []
+    from sklearn.model_selection import train_test_split
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.20)
+    X_train.shape, X_test.shape, y_train.shape
+
+    # SVM Model
+    print("SVM")
+    from sklearn import svm
+
+    lin_clf = svm.LinearSVC()
+    lin_clf.fit(X_train, y_train)
+    predict_svm = lin_clf.predict(X_test)
+    svm_acc = accuracy_score(y_test, predict_svm) * 100
+    print("ACCURACY")
+    print(svm_acc)
+    print("CLASSIFICATION REPORT")
+    print(classification_report(y_test, predict_svm))
+    print("CONFUSION MATRIX")
+    print(confusion_matrix(y_test, predict_svm))
+    detection_accuracy.objects.create(names="SVM", ratio=svm_acc)
+
+
+    print("Logistic Regression")
+
+    from sklearn.linear_model import LogisticRegression
+
+    reg = LogisticRegression(random_state=0, solver='lbfgs').fit(X_train, y_train)
+    y_pred = reg.predict(X_test)
+    print("ACCURACY")
+    print(accuracy_score(y_test, y_pred) * 100)
+    print("CLASSIFICATION REPORT")
+    print(classification_report(y_test, y_pred))
+    print("CONFUSION MATRIX")
+    print(confusion_matrix(y_test, y_pred))
+    detection_accuracy.objects.create(names="Logistic Regression", ratio=accuracy_score(y_test, y_pred) * 100)
+
+    print("Decision Tree Classifier")
+    dtc = DecisionTreeClassifier()
+    dtc.fit(X_train, y_train)
+    dtcpredict = dtc.predict(X_test)
+    print("ACCURACY")
+    print(accuracy_score(y_test, dtcpredict) * 100)
+    print("CLASSIFICATION REPORT")
+    print(classification_report(y_test, dtcpredict))
+    print("CONFUSION MATRIX")
+    print(confusion_matrix(y_test, dtcpredict))
+    detection_accuracy.objects.create(names="Decision Tree Classifier", ratio=accuracy_score(y_test, dtcpredict) * 100)
+
+
+    print("KNeighborsClassifier")
+    from sklearn.neighbors import KNeighborsClassifier
+    kn = KNeighborsClassifier()
+    kn.fit(X_train, y_train)
+    knpredict = kn.predict(X_test)
+    print("ACCURACY")
+    print(accuracy_score(y_test, knpredict) * 100)
+    print("CLASSIFICATION REPORT")
+    print(classification_report(y_test, knpredict))
+    print("CONFUSION MATRIX")
+    print(confusion_matrix(y_test, knpredict))
+    models.append(('KNeighborsClassifier', kn))
+    detection_accuracy.objects.create(names="KNeighborsClassifier", ratio=accuracy_score(y_test, knpredict) * 100)
+
+    print("Random Forest Classifier")
+    from sklearn.ensemble import RandomForestClassifier
+    rf_clf = RandomForestClassifier()
+    rf_clf.fit(X_train, y_train)
+    rfpredict = rf_clf.predict(X_test)
+    print("ACCURACY")
+    print(accuracy_score(y_test, rfpredict) * 100)
+    print("CLASSIFICATION REPORT")
+    print(classification_report(y_test, rfpredict))
+    print("CONFUSION MATRIX")
+    print(confusion_matrix(y_test, rfpredict))
+    models.append(('RandomForestClassifier', rf_clf))
+    detection_accuracy.objects.create(names="Random Forest Classifier", ratio=accuracy_score(y_test, rfpredict) * 100)
+
+    labeled = 'Predicted_data.csv'
+    df.to_csv(labeled, index=False)
+    df.to_markdown
+
+    obj = detection_accuracy.objects.all()
+    return render(request,'SProvider/train_model.html', {'objs': obj})
